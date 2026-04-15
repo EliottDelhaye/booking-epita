21826ca578f147370bd6511d5828fb69fd9ed57f
Eliott.D <eliott.delhaye@epita.fr>
2026-04-15 14:49:34 +0200

    feat: add login form

--- src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
@@ -27,12 +27,12 @@ public class SecurityConfiguration {
         return http
             .authorizeHttpRequests(auth -> auth
                 // Step 4a: add access control
-                // ...
+                .requestMatchers("/dashboard").hasRole("ADMIN")
                 // Step 4a: end
                 .anyRequest().permitAll()
             )
             // Step 4b: Add login form
-            // ...
+            .formLogin(withDefaults())
             // Step 4b: End of login form configuration
             
             .csrf((csrf) -> csrf
@@ -47,7 +47,22 @@ public class SecurityConfiguration {
     }
 
     // Step 3: add InMemoryUserDetailsManager
-    // ...
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails administrator = User.builder()
+            .username("admin")
+            .password("{bcrypt}$2a$10$g0CayepbF7qzt.Caw5Jwzu6YIpScFrCytRgQIgyW6bgSyt2JZX4te")
+            .roles("ADMIN")
+            .build();
+
+        UserDetails guest = User.builder()
+            .username("guest")
+            .password("{bcrypt}$2a$10$kdCjArfO75te9ergv6HJSul3C.WIT/GrrsZ6FAVuTpk2WQOOtQuEa")
+            .roles("GUEST")
+            .build();
+
+        return new InMemoryUserDetailsManager(administrator, guest);
+    }
     // Step 3: end
 
 }
